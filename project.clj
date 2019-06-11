d95df51e0c072a3eeeb31121b42219067f5cb796
Simon Katz <sk@nomistech.com>
2019-06-11 13:52:13 +0100

    Upgrade dependencies and add to .gitignore

--- project.clj
+++ project.clj
@@ -1,3 +1,4 @@
 (defproject skillsmatter-overtone-examples "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
-  :dependencies [[overtone "0.7.0-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [overtone "0.10.6"]])
